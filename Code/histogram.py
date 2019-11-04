17e197b2d6bc2a54ca1cc9753780f97edfc04fc4
Alanna <alanna.noguchi@students.makeschool.com>
2019-11-04 14:13:41 -0800

    Fixed code

--- Code/histogram.py
+++ Code/histogram.py
@@ -1,38 +1,47 @@
-import os
-os.system("python source_text.txt")
+import os 
+import re
+# if you are messing with files ouside of the actual app, you have to import os
+
 
 
 def histogram_dictionary(source_text):
     """Reads source_text and returns a dictionary (as a histogram) of the words"""
-    histogram = {}
-    for word in source_text:
-        word = word.lower()
-        if word in histogram:
-            histogram[word] += 1
-        else:
-            histogram[word] = 1
-
+    lines = data.readlines()
+    # print(lines)
+    # histogram = {}
+    for line in lines:
+        # remove_chars = line.rstrip(["!?,.;:"])
+        # words = line.split(" ")
+        line = re.sub(r"[^a-z0-9]"," ",line.lower())
+        words = line.split(' ')
+        for word in words:
+            
+            if word in histogram:
+                histogram[word] = histogram.get(word, 0) + 1
+            else:
+                histogram[word] = 1
     return histogram
+        
+        
+
+def unique_words(dict):
+    # return len(dict)
+    print(len(dict))
 
 
-def histogram_lists(source_text):
-    histogram = []
-    added_word = True
-
-    for word in source_text:
-        for item in histogram:
-            if item[0] == word:
-                item[1] += 1
-                added_word = False
-        if added_word is True:
-            histogram.append([word,1])
-        added_word = True
-
-    return histogram
-
+def frequency(word, dict):
+    # TODO: print the value of the key
+    for value in histogram:
+        # return histogram[value]
+        print (value, histogram[value])
 
 
 
 
 if __name__ == '__main__':
-    text = "source_text.txt"
+    histogram = {}
+    with open("sample.txt", "r") as data:
+        histogram_dictionary(data)
+        unique_words(histogram)
+        frequency("is", histogram)
+   
